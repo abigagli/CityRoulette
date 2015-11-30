28c8ea514512ea3bc0c6c96c546b8b81229f1fb8
Andrea Bigagli <abigagli@gmail.com>
2015-11-30 22:45:50 +0100

    Kicked off ShowCitesViewController

--- CityRoulette/ShowCitiesViewController.swift
+++ CityRoulette/ShowCitiesViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import MapKit
+import CoreData
 
 class ShowCitiesViewController: UIViewController {
 
+    //MARK:- Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK:- Actions
     @IBAction func doneTapped(sender: UIBarButtonItem) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    //MARK:- State
+    
+    //MARK:- Lifetime
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.mapView.delegate = self
+        self.mapView.userInteractionEnabled = false
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.fetchedResultsController.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -24,6 +43,24 @@ class ShowCitiesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: Core Data
+    private lazy var sharedContext: NSManagedObjectContext = {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    }()
+    
+    private lazy var fetchedResultsController: NSFetchedResultsController = {
+        
+        //create fetch request with sort descriptor
+        let fetchRequest = NSFetchRequest(entityName: "CityInfo")
+        //fetchRequest.predicate = NSPredicate(format: "mapPin == %@", self.pin)
+        fetchRequest.sortDescriptors = []
+        
+        //create controller from fetch request
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.sharedContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        //fetchedResultsController.delegate = self
+        return fetchedResultsController
+    }()
 
     /*
     // MARK: - Navigation
@@ -34,5 +71,53 @@ class ShowCitiesViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+//MARK:- Protocol Conformance
+
+//MARK: MKMapViewDelegate
+extension ShowCitiesViewController: MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "travelLocationPin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.pinTintColor = UIColor.greenColor()
+            
+            pinView!.animatesDrop = false
+            
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+}
+
+//MARK: UITableViewDataSource, UITableViewDelegate
+extension ShowCitiesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cityInfoCell", forIndexPath: indexPath)
+
+        cell.textLabel?.text = "text"
+
+        cell.detailTextLabel?.text = "detail"
+        
+        return cell
+    }
+    
+}
+
+//MARK: NSFetchedResultsControllerDelegate
+extension ShowCitiesViewController: NSFetchedResultsControllerDelegate {
 
 }
